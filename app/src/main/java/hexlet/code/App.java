6383d449a0925d85fb5d3b88dc04761a24cea764
MaryKom <masha.komendantova@yandex.ru>
2022-11-29 18:54:36 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,6 +8,13 @@ import picocli.CommandLine.Parameters;
         description = "Compares two configuration files and shows a difference.")
 public class App {
 
+    @Parameters(index = "0", description = "path to first file")
+    private static String filepath1;
+    @Parameters(index = "1", description = "path to second file")
+    private static String filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
