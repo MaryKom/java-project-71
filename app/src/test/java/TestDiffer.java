3d9bba720cdf4cc8d235f3d00148a8686fe3752c
MaryKom <masha.komendantova@yandex.ru>
2022-12-04 17:18:21 +0300

    add test

--- app/src/test/java/TestDiffer.java
+++ app/src/test/java/TestDiffer.java
@@ -1,9 +1,10 @@
 import hexlet.code.Differ;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestDiffer {
     private final String filepath1Json = ("src/test/resources/file1.json");
@@ -22,6 +23,24 @@ public class TestDiffer {
     public void generateTestDefaultYAML() throws Exception {
         String actual = Differ.generate(filepath1Yml, filepath2Yml);
         String expected = Files.readString(Paths.get("src/test/resources/stylish_yml"));
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void generateTestJson2() throws Exception {
+        String filepath3Json = ("src/test/resources/file3.json");
+        String filepath4Json = ("src/test/resources/file4.json");
+        String expected = Files.readString(Paths.get("src/test/resources/stylish_json_2"));
+        String actual = Differ.generate(filepath3Json, filepath4Json);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void generateTestYAML2() throws Exception {
+        String filepath3Yml = ("src/test/resources/file3.yml");
+        String filepath4Yml = ("src/test/resources/file4.yml");
+        String actual = Differ.generate(filepath3Yml, filepath4Yml);
+        String expected = Files.readString(Paths.get("src/test/resources/stylish_yml_2"));
         assertThat(actual).isEqualTo(expected);
     }
 }
